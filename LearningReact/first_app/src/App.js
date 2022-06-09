7050a83c48001185f5dfb3454aef83ddd255aedc
Satish Kumar <sksatishk9@gmail.com>
2022-06-10 00:00:01 +0530

    commit

--- LearningReact/first_app/src/App.js
+++ LearningReact/first_app/src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
-
+import Expenses from "./components/Expenses";
 function App() {
+  const expenses = [
+    { 
+      title: "Car Servicing", 
+      amount: 500, 
+      date: new Date(2020, 2, 20)
+     },
+    { 
+      title: "Car Insurance", 
+      amount: 275, 
+      date: new Date(2022, 12, 16) 
+    },
+    { 
+      title: "Grocery Items",
+      amount: 100, 
+      date: new Date(2021, 3, 24) 
+    },
+    { 
+      title: "Books", 
+      amount: 350, 
+      date: new Date(2019, 5, 14) 
+    },
+  ];
+
   return (
     <div className="App">
       <h1>Let's get started!</h1>
+      <Expenses expenses />
+
     </div>
   );
 }
